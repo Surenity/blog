34f58f6c55b3ae78553bca755f0ffff86381f359
Surenity Brink <surenitybrink@gmail.com>
2018-09-05 15:52:40 -0700

    again

--- main.js
+++ main.js
@@ -3,3 +3,7 @@
       console.log(response)
       const blogContainer = document.querySelector('#blog-go-here')
     })
+
+    for (let i = 0; i < response.data.length; i ++){
+      console.log(response.data)
+    }
