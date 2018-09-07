e1370d418c5ab7a066a6d00e80cdcd3405240eb5
Surenity Brink <surenitybrink@gmail.com>
2018-09-07 11:13:31 -0700

    trying to figure out how to append everything to the page

--- main.js
+++ main.js
@@ -4,6 +4,17 @@
         for (let i = 0; i < response.data.length; i ++){
           console.log(response.data)
           // something needs to go here to look through the responce to pull out all the blogs
+          // 1. Create desired element
+          // 2. Configure said element
+          // 3. Append element to parent
+
+          // let title = document.createElement('div')
+
+          let random = document.createElement("p")
+          let textrandom = document.createTextNode("words and things")
+          random.appendChild(textrandom)
+          document.getElementById("blogContainer").appendChild(random)
+
 
         }
       // console.log(response)
