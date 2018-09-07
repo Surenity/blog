fb19f11bd8949cb079d2fea91cd3391c0c4facb6
Surenity Brink <surenitybrink@gmail.com>
2018-09-07 11:34:00 -0700

    again

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
     .then(function (response) {
       const blogContainer = document.querySelector('#blog-go-here')
         for (let i = 0; i < response.data.length; i ++){
-          console.log(response.data)
+          // console.log(response.data)
           // something needs to go here to look through the responce to pull out all the blogs
           // 1. Create desired element
           // 2. Configure said element
@@ -13,8 +13,8 @@
           let random = document.createElement("p")
           let textrandom = document.createTextNode("words and things")
           random.appendChild(textrandom)
-          document.getElementById("blogContainer").appendChild(random)
-
+          document.getElementById("blogContainer").appendChild(textrandom)
+            console.log(blogContainer)
 
         }
       // console.log(response)
